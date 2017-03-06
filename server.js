c2627abd7ea2c57889114bcc7eaaf2bac85ab1a6
RAJJVAL <RAJJVALKJAIN@GMAIL.COM>
2017-03-07 00:05:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ function createTemplate(data){
     var date=data.date;
     var heading=data.heading;
     var content=data.content;
-}
+
 var htmlTemplate= `<html>
      <head>
         <title>
@@ -61,7 +61,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function(req,res) {
-   res.sendfile(path.join(__dirname,'ui','article-one.html')); 
+   res.send(createTemplate(articleOne)); 
 });
 
 app.get('/article-two', function(req,res) {
