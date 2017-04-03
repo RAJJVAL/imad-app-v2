52122fcc3f81c991e3f3c3aa88f2b218ffc9732a
RAJJVAL <RAJJVALKJAIN@GMAIL.COM>
2017-04-03 17:30:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -60,6 +60,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'hai.html'));
+});
+
+
 app.get('/article-one', function(req,res) {
    res.send(createTemplate(articleOne)); 
 });
