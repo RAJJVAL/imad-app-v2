8a39ad156f40c955f6a9ce08f34bc1c19ff662a7
RAJJVAL <RAJJVALKJAIN@GMAIL.COM>
2017-04-09 13:44:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,52 +1,24 @@
-var button = document.getElementById('counter');
-
-
-button.onclick = function(){
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function(){
-        if(request.readState === XMLHttpRequest.DONE){
-            if(request.status === 200){
-                var names = request.responseText;
-                names = JSON.parse(names);
-                var list='';
-                for(var i=0;i<names.length;i++)
-                {
-                    list += '<li>' + names[i] + '</li>'
-                }
-                var ul=getElementById('namelist');
-                ul.innerHTML = list;
-            }
-        } 
-}
-request.open('GET','https://rajjval@ssh.imad.hasura-app.io/submit-name?name=' + name, true);
-request.send(null);
-
-
-};
-
-
-
 var submit = document.getElementById('submit-btn');
 submit.onclick = function(){
     var request = new XMLHttpRequest();
     request.onreadystatechange = function(){
         if(request.readState === XMLHttpRequest.DONE){
             if(request.status === 200){
-                var names = request.responseText;
-                names = JSON.parse(names);
-                var list='';
-                for(var i=0;i<names.length;i++)
-                {
-                    list += '<li>' + names[i] + '</li>'
-                }
-                var ul=getElementById('namelist');
-                ul.innerHTML = list;
+                console.log('user logged in');
+                alert('logged in successifully');
+            }else if(request.status === 403){
+                alert('username/password is incorrect');
+            }else if(request.status === 500){
+                alert('something went wrong on the server');
             }
         }
     };
     
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
-request.open('GET','https://rajjval@ssh.imad.hasura-app.io/submit-name?name=' + name, true);
-request.send(null);
+var username = document.getElementById('username').value;
+var password = document.getElementById('password').value;
+console.log(username);
+console.log(password);
+request.open('POST','https://rajjval@ssh.imad.hasura-app.io/login', true);
+request.setRequestHeader('Content-type','application/json');
+request.send(JSON.stringify({username: username, password: password}));
 };
