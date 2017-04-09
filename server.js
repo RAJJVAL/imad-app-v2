a61e4143b4a658bb377e69abef55dc4e52ebdeab
RAJJVAL <RAJJVALKJAIN@GMAIL.COM>
2017-04-09 14:06:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ app.get('/', function (req, res) {
 
 function hash (input, salt){
 var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
-    return ["pdkdf","10000",salt,hashed.toString('hex')].join('$');
+    return hashed.toString('hex');
 }
 
 
